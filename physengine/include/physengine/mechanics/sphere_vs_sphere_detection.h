b63f003b4427260705ecc63b7dad9d6b5738e167
Hogne <hsu014@post.uit.no>
2025-03-14 12:42:25 +0100

    Sphere vs sphere detection & response

--- physengine/include/physengine/mechanics/sphere_vs_sphere_detection.h
+++ physengine/include/physengine/mechanics/sphere_vs_sphere_detection.h
@@ -23,7 +23,52 @@ namespace dte3607::physengine::mechanics
     [[maybe_unused]] types::HighResolutionTP const& t_0,
     [[maybe_unused]] types::Duration                timestep)
   {
-    return {};
+
+    if (s1_tc != s2_tc) {
+      std::cout << "Spheres not synced in time, fix code! " << std::endl;
+    }
+
+    types::Duration time_1 = timestep - (s1_tc - t_0);
+    types::Duration time_2 = timestep - (s2_tc - t_0);
+
+    types::ValueType r = s1_r + s2_r;
+    types::ValueType r_sqr = pow(r, 2);
+    auto ds_1 = computeLinearTrajectory(s1_v, external_forces, time_1).first;
+    auto ds_2 = computeLinearTrajectory(s2_v, external_forces, time_2).first;
+
+    auto Q = s2_p - s1_p;
+    auto R = ds_2 - ds_1;
+    auto eps = 1e-5;
+
+    auto QQ_inner = blaze::inner(Q, Q);
+    auto RR_inner = blaze::inner(R, R);
+    auto QR_inner = blaze::inner(Q, R);
+    auto inside = pow(QR_inner, 2) - RR_inner * (QQ_inner - r_sqr); // Part inside square root
+
+
+    if (QQ_inner - r_sqr < eps) {
+      // Spheres are touching
+      return std::nullopt;
+    }
+
+    if (RR_inner < eps) {
+      // Spheres are moving (almost) in parallel
+      return std::nullopt;
+    }
+
+    if (inside < 0) {
+      // No collision
+      return std::nullopt;
+    }
+
+    auto x = (-QR_inner - sqrt(pow(QR_inner, 2) - RR_inner * ( QQ_inner - r_sqr ))) / RR_inner;
+    if (x<=0 or x>1) {
+      // Collision outside of timestep
+      return std::nullopt;
+    }
+
+    auto const tp = utils::toDuration(x * time_1);
+    return t_0 + tp;
   }
 
 
--- physengine/include/physengine/mechanics/sphere_vs_sphere_response.h
+++ physengine/include/physengine/mechanics/sphere_vs_sphere_response.h
@@ -19,7 +19,27 @@ namespace dte3607::physengine::mechanics
       [[maybe_unused]] types::Vector3 const& s2_v,
       [[maybe_unused]] types::ValueType      s2_mass)
   {
-    return {};
+
+    auto d = blaze::normalize(s2_p - s1_p);
+
+    auto v1_d = blaze::inner(s1_v, d);
+    auto v2_d = blaze::inner(s2_v, d);
+
+    auto v1_n = s1_v - v1_d * d;
+    auto v2_n = s2_v - v2_d * d;
+
+    auto m = s1_mass + s2_mass;
+
+    auto new_v1_d = (s1_mass - s2_mass) / m * v1_d +
+                    2 * s2_mass / m * v2_d;
+
+    auto new_v2_d = (s2_mass - s1_mass) / m * v2_d +
+                    2 * s1_mass / m * v1_d;
+
+    auto new_v1 = v1_n + new_v1_d * d;
+    auto new_v2 = v2_n + new_v2_d * d;
+
+    return {new_v1, new_v2};
   }
 
 
