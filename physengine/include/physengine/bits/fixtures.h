6246f59654e90d8aab68be338def08255d580636
Hogne <hsu014@post.uit.no>
2025-04-01 15:20:48 +0200

    Fixture level 3

--- physengine/include/physengine/bits/fixtures.h
+++ physengine/include/physengine/bits/fixtures.h
@@ -100,11 +100,11 @@ namespace dte3607::physengine::fixtures
     /*** API concept required methods ***/
 
     // Environment
-    void setGravity(Forces G) {
+    void      setGravity(Forces G) {
       m_gravity = G;
     }
 
-    Forces getGravity() const {
+    Forces    getGravity() const {
       return m_gravity;
     }
 
@@ -112,6 +112,7 @@ namespace dte3607::physengine::fixtures
     size_t    noRigidBodies() const {
       return m_rigid_bodies.size();
     }
+
     std::vector<size_t> nonFixedSphereRBs() const {
       return m_sphere_idx;
     }
@@ -120,6 +121,7 @@ namespace dte3607::physengine::fixtures
       return m_plane_idx;
     }
 
+    // RB properties
     ValueType rbSphereRadius(size_t s_rid) const {
       return m_rigid_bodies[s_rid].radius.value();
     }
@@ -132,17 +134,13 @@ namespace dte3607::physengine::fixtures
       return m_rigid_bodies[p_rid].normal.value();
     }
 
-    // RB properties
-    types::Point3 globalFramePosition(size_t rid) const
-    {
+    Point3    globalFramePosition(size_t rid) const {
       return m_rigid_bodies[rid].translation;
     }
 
-    void setGlobalFramePosition(size_t rid, types::Vector3 pos)
-    {
+    void      setGlobalFramePosition(size_t rid, Vector3 pos) {
       m_rigid_bodies[rid].translation = pos;
     }
-
 
 
 
@@ -184,17 +182,17 @@ namespace dte3607::physengine::fixtures
     /*** END API requirements ***/
 
     struct RigidBody {
-      std::optional<types::ValueType> radius;
-      std::optional<types::Vector3>   velocity;
-      types::Vector3                  translation;
-      std::optional<types::Vector3>   normal;
+      std::optional<ValueType> radius;
+      std::optional<Vector3>   velocity;
+      Vector3                  translation;
+      std::optional<Vector3>   normal;
     };
 
     /*** Members ***/
-    std::vector<RigidBody> m_rigid_bodies;
-    std::vector<size_t> m_sphere_idx;
-    std::vector<size_t> m_plane_idx;
-    Forces m_gravity;
+    std::vector<RigidBody>     m_rigid_bodies;
+    std::vector<size_t>        m_sphere_idx;
+    std::vector<size_t>        m_plane_idx;
+    Forces                     m_gravity;
   };
 
 
@@ -222,34 +220,180 @@ namespace dte3607::physengine::fixtures
     /*** API concept required methods ***/
 
     // Environment
-    void setGravity(Forces) {}
+    void      setGravity(Forces G) {
+      m_gravity = G;
+    }
 
-    ValueType rbPlaneMaxFrictionCoef() const { return {}; }
-    ValueType rbSphereMaxFrictionCoef() const { return {}; }
+    Forces    getGravity() const {
+      return m_gravity;
+    }
 
-    RBState rbState([[maybe_unused]] size_t rid) const { return {}; }
+    // RBs
+    size_t    noRigidBodies() const {
+      return m_rigid_bodies.size();
+    }
+
+    std::vector<size_t> nonFixedSphereRBs() const {
+      return m_sphere_idx;
+    }
+
+    std::vector<size_t> fixedInfPlaneRBs() const {
+      return m_plane_idx;
+    }
+
+    // RB properties
+    void      setGlobalFramePosition(size_t rid, Vector3 pos) {
+      m_rigid_bodies[rid].translation = pos;
+    }
+
+    Point3    globalFramePosition(size_t rid) const {
+      return m_rigid_bodies[rid].translation;
+    }
+
+    ValueType rbSphereRadius(size_t s_rid) const {
+      return m_rigid_bodies[s_rid].radius.value();
+    }
+
+    void setSphereVelocity(size_t rid, Vector3 v) {
+      m_rigid_bodies[rid].velocity = v;
+    }
+
+    Vector3   rbSphereVelocity(size_t s_rid) const {
+      return m_rigid_bodies[s_rid].velocity.value();
+    }
+
+    RBState rbInitialState(size_t s_rid) const {
+      return m_rigid_bodies[s_rid].initial_state.value();
+    }
+
+    ValueType rbFrictionCoef (size_t rid) const {
+      return m_rigid_bodies[rid].friction_coef;
+    }
+
+    ValueType rbMass (size_t s_rid) const {
+      return m_rigid_bodies[s_rid].mass.value();
+    }
+
+    Vector3   rbPlaneNormal(size_t p_rid) const {
+      return m_rigid_bodies[p_rid].normal.value();
+    }
+
+    ValueType rbPlaneMaxFrictionCoef() const {
+      /*
+      // ValueType max_friction = 0.;
+      // for (size_t id : m_plane_idx) {
+      //   if (m_rigid_bodies[id].friction_coef > max_friction) {
+      //     max_friction = m_rigid_bodies[id].friction_coef;
+      //   }
+      // }
+      // return max_friction; */
+      return m_max_friction;
+    }
+
+    ValueType rbSphereMaxFrictionCoef() const {
+      /*
+      //ValueType max_friction = 0.;
+      // for (size_t id : m_sphere_idx) {
+      //   if (m_rigid_bodies[id].friction_coef > max_friction) {
+      //     max_friction = m_rigid_bodies[id].friction_coef;
+      //   }
+      // }
+      // return max_friction; */
+      return m_max_friction;
+    }
+
+    void      setRbState (size_t rid, RBState state) {
+      m_rigid_bodies[rid].initial_state = state;
+    }
+
+    RBState   rbState(size_t rid) const {
+
+      if (m_rigid_bodies[rid].initial_state.has_value()) {
+        return m_rigid_bodies[rid].initial_state.value();
+      }
+      return {};
+    }
+
+    // Attachements
+    void clearAttached() {
+      m_attached.clear();
+    }
+
+    std::vector<std::pair<size_t, size_t>>
+    getAttached() {
+      return m_attached;
+    }
+
+    void addAttached(size_t s_id, size_t p_id) {
+      m_attached.push_back({s_id, p_id});
+    }
+
 
 
     /*** Fixture unit-test setup API ***/
 
-    size_t createSphere([[maybe_unused]] ValueType radius      = 1.,
-                        [[maybe_unused]] Vector3   velocity    = {0, 0, 0},
-                        [[maybe_unused]] Vector3   translation = {0, 0, 0},
-                        [[maybe_unused]] RBState initial_state = RBState::Free,
-                        [[maybe_unused]] ValueType friction_coef = 0.,
-                        [[maybe_unused]] ValueType mass          = 1.)
+    size_t createSphere(ValueType radius        = 1.,
+                        Vector3   velocity      = {0, 0, 0},
+                        Vector3   translation   = {0, 0, 0},
+                        RBState   initial_state = RBState::Free,
+                        ValueType friction_coef = 0.,
+                        ValueType mass          = 1.)
     {
-      return {};
+      m_rigid_bodies.push_back({
+        radius,
+        velocity,
+        translation,
+        initial_state,
+        friction_coef,
+        mass,
+        std::nullopt
+      });
+
+      auto id = m_rigid_bodies.size() - 1;
+      m_sphere_idx.push_back(id);
+
+      return id;
     }
 
-    size_t createFixedInfPlane([[maybe_unused]] Vector3 normal      = {0, 1, 0},
-                               [[maybe_unused]] Vector3 translation = {0, 0, 0},
-                               [[maybe_unused]] ValueType friction_coef = 0.)
+    size_t createFixedInfPlane(Vector3   normal        = {0, 1, 0},
+                               Vector3   translation   = {0, 0, 0},
+                               ValueType friction_coef = 0.)
     {
-      return {};
+      m_rigid_bodies.push_back({
+        std::nullopt,
+        std::nullopt,
+        translation,
+        std::nullopt,
+        friction_coef,
+        std::nullopt,
+        normal
+      });
+
+      auto id = m_rigid_bodies.size() - 1;
+      m_plane_idx.push_back(id);
+
+      return id;
     }
 
     /*** END API requirements ***/
+
+    struct RigidBody {
+      std::optional<ValueType> radius;
+      std::optional<Vector3>   velocity;
+      Vector3                  translation;
+      std::optional<RBState>   initial_state;
+      ValueType                friction_coef;
+      std::optional<ValueType> mass;
+      std::optional<Vector3>   normal;
+    };
+
+    /*** Members ***/
+    std::vector<RigidBody>     m_rigid_bodies;
+    std::vector<size_t>        m_sphere_idx;
+    std::vector<size_t>        m_plane_idx;
+    Forces                     m_gravity;
+    ValueType                  m_max_friction = 1;
+    std::vector<std::pair<size_t, size_t>> m_attached;
   };
 
 
